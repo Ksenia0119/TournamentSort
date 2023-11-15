1477e68e6e8ea39a56fa91edc53a3eaf7c99f761
Ksenia Maltseva <ksenya.maltseva1901@inbox.ru>
2023-11-15 13:34:20 +0900

    Добавьте файлы проекта.

---
+++ TourSort/TourSort.cpp
@@ -0,0 +1,25 @@
+﻿//@author Maltseva K.V.
+
+
+#include <iostream>
+#include <array>
+#include "TourSort.h"
+using namespace std;
+
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    int arr[] = { 17 ,28,99,255,123,78,2,5,67};
+    int arrLength = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Массив: ";
+    printArr(arr, arrLength);
+
+    tournamentSort(arr, arrLength);
+    cout << endl;
+    cout << "Отсортированный массив: ";
+    printArr(arr, arrLength);
+
+    cout << endl;
+    testTournamentSort();
+}
